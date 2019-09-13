1a6ef7ce9b53bf24e1e8bd01925d76a9977b36be
Andy <amaggio@glimpsegroup.io>
2019-09-13 12:25:30 -0400

    9.13 Commit

---
+++ Assets/Scripts/Kill_Scene_Head_Collider_Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Top_Plane_Collider_Trigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // add isTrigger
+        var boxCollider = GameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        void onTriggerEnter(Collider other) {
+            if (other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(0);
+
+            }
+}
